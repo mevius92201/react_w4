06c3e2584612b8678446fba6ba85dbc79882aaf4
Chun <mevius92201@gmail.com>
2025-02-10 11:21:23 +0800

    add ProductPage component and refactor App component to integrate it

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,35 @@ import LoginPage from './component/Pages/LoginPage';
 import Pagination from './component/Pagination';
 import ProductModal from './component/ProductModal';
 import DeleteModal from "./component/DeleteModal";
+import ProductPage from "./component/Pages/ProductPage";
 
 const API_BASE = "https://ec-course-api.hexschool.io/v2";
 const API_PATH = "mevius"; 
 
-const defaultModal = {
-  imageUrl: "",
-  title: "",
-  category: "",
-  unit: "",
-  origin_price: 0,
-  price: 0,
-  id: "",
-  description: "",
-  content: "",
-  is_enabled: false,
-  imagesUrl: []
-}
+
 function App() {
 
   const [isAuth, setIsAuth] = useState(false);
-  const [products, setProducts] = useState([]);
   const myModalRef = useRef(null);
-  const [modalState, setModalState] = useState(null);
-  const [productModal, setProductModal] = useState(defaultModal);
   const myDeleteModalRef = useRef(null);
- 
- 
+  const [isProductModalOpen, setIsProductModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [modalState, setModalState] = useState(null); 
+  const [products, setProducts] = useState([]);
+  const defaultModal = {
+    imageUrl: "",
+    title: "",
+    category: "",
+    unit: "",
+    origin_price: 0,
+    price: 0,
+    id: "",
+    description: "",
+    content: "",
+    is_enabled: false,
+    imagesUrl: []
+}
+const [productModal, setProductModal] = useState(defaultModal);
   //const [inputImageValue, setInputImageValue] = useState("");
   //const [inputImagesValue, setInputImagesValue] = useState("");
  //const [currentPage, setCurrentPage] = useState(1)
@@ -80,105 +82,23 @@ function App() {
   //     console.log('modalRef.current is null')
   //   }
   // }
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-
-
-  const hasDeleteModalShow = (item) =>{
-    setProductModal({
-      ...item,
-      imagesUrl: [],
-    });
-    if (myDeleteModalRef.current !== null){
-      //myDeleteModalRef.current.show();
-      setIsDeleteModalOpen(true);
-    } else {
-      console.log('modalRef.current is null')
-    }
-  }
-
-  const [isProductModalOpen, setIsProductModalOpen] = useState(false);
-
-  const hasModalShow = (state, item) =>{
-    setProductModal(state);
-    if (state === 'add'){
-      setProductModal(
-        {...defaultModal})
-        setModalState('add')
-    } else {
-      setProductModal(item)
-      setModalState('edit')
-    }
-    if (myModalRef.current !== null){
-      setIsProductModalOpen(true);
-    } else {
-      console.log('modalRef.current is null')
-    }
-}
+  
 
   return (
     <>
       {isAuth ? (
-        <div>
-          <div className="container">
-            <div className="text-end mt-4">
-              <button className="btn btn-primary" 
-              onClick={() => 
-                {hasModalShow('add');
-                setProductModal(defaultModal);
-                }
-                }>建立新的產品</button>
-            </div>
-            <table className="table mt-4">
-              <thead>
-                <tr>
-                  <th width="120">分類</th>
-                  <th>產品名稱</th>
-                  <th width="120">原價</th>
-                  <th width="120">售價</th>
-                  <th width="100">是否啟用</th>
-                  <th width="120">編輯</th>
-                </tr>
-              </thead>
-              <tbody>
-                {products.map((item) => (
-                  <tr key={item.id}>
-                    <td>{item.category}</td>
-                    <td>{item.title}</td>
-                    <td className="text-end">{item.origin_price}</td>
-                    <td className="text-end">{item.price}</td>
-                    <td>
-                      {item.is_enabled ? (
-                        <span className="text-success">啟用</span>
-                      ) : (
-                        <span>未啟用</span>
-                      )}
-                    </td>
-                    <td>
-                      <div className="btn-group">
-                        <button
-                          type="button"
-                          className="btn btn-outline-primary btn-sm"
-                          onClick={() =>{hasModalShow(`edit`, item);}}
-                          
-                        >
-                          編輯
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-outline-danger btn-sm"
-                          onClick={() =>{hasDeleteModalShow(item)}}
-                        >
-                          刪除
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          < Pagination getProducts={getProducts} pagination={pagination}/>
-        </div>
+        <ProductPage 
+        myModalRef={myModalRef}
+        setIsProductModalOpen={setIsProductModalOpen}
+        getProducts={getProducts}
+        setIsDeleteModalOpen={setIsDeleteModalOpen}
+        pagination={pagination}
+        myDeleteModalRef={myDeleteModalRef}
+        setModalState={setModalState}
+        setProductModal={setProductModal}
+        defaultModal={defaultModal}
+        products={products}
+        />
       ) : <LoginPage getProducts={getProducts} setIsAuth={setIsAuth}/> }
       <ProductModal 
       modalState={modalState}
--- src/component/Pages/ProductPage.jsx
+++ src/component/Pages/ProductPage.jsx
@@ -1,4 +1,100 @@
-function ProductPage(){
+
+import { useState} from 'react';
+import Pagination from '../Pagination';
+
+function ProductPage({myModalRef, myDeleteModalRef, setIsProductModalOpen, setIsDeleteModalOpen, getProducts, pagination, setModalState, setProductModal, defaultModal, products}){
     
+    const hasModalShow = (state, item) =>{
+      setProductModal(state);
+      if (state === 'add'){
+        setProductModal({...defaultModal})
+        setModalState('add')
+      } else {
+        setProductModal(item)
+        setModalState('edit')
+      }
+      if (myModalRef.current !== null){
+        setIsProductModalOpen(true);
+      } else {
+        console.log('modalRef.current is null')
+      }
+    }
+    const hasDeleteModalShow = (item) =>{
+        setProductModal({
+          ...item,
+          imagesUrl: [],
+        });
+        if (myDeleteModalRef.current !== null){
+          //myDeleteModalRef.current.show();
+          setIsDeleteModalOpen(true);
+        } else {
+          console.log('modalRef.current is null')
+        }
+    }
+
+    return(
+        <div>
+            <div className="container">
+            <div className="text-end mt-4">
+                <button className="btn btn-primary" 
+                onClick={() => 
+                {hasModalShow('add');
+                setProductModal(defaultModal);
+                }
+                }>建立新的產品</button>
+            </div>
+            <table className="table mt-4">
+                <thead>
+                <tr>
+                    <th width="120">分類</th>
+                    <th>產品名稱</th>
+                    <th width="120">原價</th>
+                    <th width="120">售價</th>
+                    <th width="100">是否啟用</th>
+                    <th width="120">編輯</th>
+                </tr>
+                </thead>
+                <tbody>
+                {products.map((item) => (
+                    <tr key={item.id}>
+                    <td>{item.category}</td>
+                    <td>{item.title}</td>
+                    <td className="text-end">{item.origin_price}</td>
+                    <td className="text-end">{item.price}</td>
+                    <td>
+                        {item.is_enabled ? (
+                        <span className="text-success">啟用</span>
+                        ) : (
+                        <span>未啟用</span>
+                        )}
+                    </td>
+                    <td>
+                        <div className="btn-group">
+                        <button
+                            type="button"
+                            className="btn btn-outline-primary btn-sm"
+                            onClick={() =>{hasModalShow(`edit`, item);}}
+                            
+                        >
+                            編輯
+                        </button>
+                        <button
+                            type="button"
+                            className="btn btn-outline-danger btn-sm"
+                            onClick={() =>{hasDeleteModalShow(item)}}
+                        >
+                            刪除
+                        </button>
+                        </div>
+                    </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            </div>
+            < Pagination getProducts={getProducts} pagination={pagination}/>
+        </div>
+    )
 }
+
 export default ProductPage;
